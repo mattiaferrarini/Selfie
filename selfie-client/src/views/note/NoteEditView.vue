2e66ed896220e8145c5a55f49683c8643fa13247
Fabio Murer <fabio.murer@studio.unibo.it>
2024-08-16 16:40:55 +0200

    markdown notes with bugs

--- selfie-client/src/views/note/NoteEditView.vue
+++ selfie-client/src/views/note/NoteEditView.vue
@@ -5,8 +5,9 @@ import userService from "@/services/userService";
 import router from "@/router";
 import { useTextareaAutosize } from '@vueuse/core'
 import VueMultiselect from 'vue-multiselect'
+import { marked } from 'marked'
 
-// declaring reactive variables variables
+// declaring reactive variables
 const { textarea: contentArea, input: content } = useTextareaAutosize();
 const { textarea: titleArea, input: title } = useTextareaAutosize();
 
@@ -18,6 +19,10 @@ const category = ref();
 const open_to_anyone = ref(false);
 const owners = ref(null);
 const users = ref([]);
+
+// markdown rendering
+const viewMode = ref(true);
+const renderedMarkdown = ref("");
 
 // useful functions
 
@@ -54,9 +59,17 @@ const getUserNames = async () => {
   users.value = await userService.getUserNames();
 };
 
+const changeViewMode = async () => {
+  viewMode.value = !viewMode.value;
+  if (viewMode.value) {
+    renderedMarkdown.value = await marked(content.value);
+  }
+};
+
 onMounted( async () => {
   await getUserNames();
   await getNote();
+  renderedMarkdown.value = await marked(content.value);
 })
 
 </script>
@@ -74,6 +87,11 @@ onMounted( async () => {
           class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700"
           @click="deleteNote()">delete
       </button>
+      <button
+          type="button"
+          class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700"
+          @click="changeViewMode">{{ viewMode ? "edit" : "view" }}
+      </button>
     </div>
     <div class="flex justify-center">
       <textarea
@@ -81,22 +99,26 @@ onMounted( async () => {
           class="w-screen max-w-screen-md text-2xl text-center resize-none"
           v-model="title"
           placeholder="edit me"
-      />
+          :disabled="viewMode">
+      </textarea>
     </div>
     <div class="flex justify-center">
-      <input type="text" v-model="category" class="text-center m-2 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700">
+      <input type="text" v-model="category" :disabled="viewMode" class="text-center m-2 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700">
     </div>
     <div class="flex flex-row flex-wrap justify-center">
       <p class="m-2">Creation date: {{ creation }}</p>
       <p class="m-2">Last modification {{ lastmodify }}</p>
     </div>
-
-    <div class="flex justify-center resize-none" >
+    <div v-if="viewMode" class="flex justify-center">
+      <div v-html='renderedMarkdown' class="w-screen max-w-screen-md bg-white"></div>
+    </div>
+    <div v-else class="flex justify-center resize-none" >
       <textarea
           ref="contentArea"
           class="w-screen max-w-screen-md min-h-[200px] resize-none"
           v-model="content"
-          placeholder="edit me">
+          placeholder="edit me"
+          :disabled="viewMode">
       </textarea>
     </div>
 
@@ -104,13 +126,14 @@ onMounted( async () => {
       <p class="text-center">Owners:</p>
       <div class="flex">
         <label for="open_to_anyone">open to anyone</label>
-        <input class="ml-1" type="checkbox" id="open_to_anyone" v-model="open_to_anyone" />
+        <input class="ml-1" type="checkbox" id="open_to_anyone" v-model="open_to_anyone" :disabled="viewMode"/>
       </div>
 
       <VueMultiselect v-if="!open_to_anyone"
           v-model="owners"
           :options="users"
-          :multiple="true">
+          :multiple="true"
+          :disabled="viewMode">
       </VueMultiselect>
 
     </div>
