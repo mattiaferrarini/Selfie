4b05e06aa4890d6a65672a7cfc0325a052c20e8d
Mattia Ferrarini <mattiaferrarini22@gmail.com>
2024-08-17 15:45:01 +0200

    Added fake timers to front-end

--- selfie-client/vue.config.js
+++ selfie-client/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "util": require.resolve("util/"),
+        "timers": require.resolve("timers-browserify")
+      }
+    }
+  }
 })
