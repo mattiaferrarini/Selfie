e7ffaeb9a1d94f2667e20b362257797d8e414e7b
Fabio Murer <fabio.murer@studio.unibo.it>
2024-08-19 18:04:57 +0200

    pretty todoList

---
+++ selfie-client/src/components/todoList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {defineModel, defineProps, ref} from "vue";
+import ActivityForm from "@/components/Calendar/ActivityForm.vue";
+
+const todoData: any = defineModel()
+defineProps<{
+  editable: boolean
+}>()
+
+const newTodo = ref("");
+
+const addTodo = () => {
+  todoData.value.push({
+    title: newTodo.value,
+    done: false
+  });
+};
+
+const makeActivity = (todo: any) => {
+  let o = 0;
+};
+
+</script>
+
+<template>
+  <div class="flex flex-col flex-wrap border-black border-2 rounded p-3">
+    <h1 class="font-bold text-2xl">Todo List</h1>
+    <ul>
+      <li v-for="todo in todoData" :key="todo.title" class="pb-3">
+        <div v-if="!todo.activityID" class="flex flex-row justify-between">
+          <input type="checkbox" v-model="todo.done" :disabled="editable">
+          <span>{{ todo.title }}</span>
+          <button class="bg-yellow-400 rounded-lg p-1" @click="makeActivity(todo)" :disabled="editable">Make Activity</button>
+        </div>
+        <div v-else>
+          <p>leses</p>
+        </div>
+      </li>
+    </ul>
+    <div class="flex flex-row pt-3 gap-x-3">
+      <input type="text" v-model="newTodo" class="w-full" :disabled="editable">
+      <button @click="addTodo" class="bg-green-700 rounded-lg p-1" :disabled="editable">Add</button>
+    </div>
+  </div>
+</template>
--- selfie-client/src/views/note/NoteEditView.vue
+++ selfie-client/src/views/note/NoteEditView.vue
@@ -27,6 +27,7 @@ const renderedMarkdown = ref("");
 
 // todoList
 const todoData = ref([]);
+const todoListShow = ref(false);
 
 // useful functions
 
@@ -83,44 +84,49 @@ onMounted( async () => {
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <div class="flex flex-row flex-wrap justify-center">
-      <button
-          type="button"
-          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700"
-          @click="saveNote()">save and close
-      </button>
-      <button
-          type="button"
-          class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700"
-          @click="deleteNote()">delete
-      </button>
-      <button
-          type="button"
-          class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700"
-          @click="changeViewMode">{{ viewMode ? "edit" : "view" }}
-      </button>
-    </div>
-    <div class="flex justify-center">
+  <div class="flex flex-col flex-wrap justify-center w-screen max-w-screen-md m-auto">
+    <div class="flex flex-col">
+      <div class="flex flex-row flex-wrap justify-center">
+        <button
+            type="button"
+            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700"
+            @click="saveNote()">save and close
+        </button>
+        <button
+            type="button"
+            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700"
+            @click="deleteNote()">delete
+        </button>
+        <button
+            type="button"
+            class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700"
+            @click="changeViewMode">{{ viewMode ? "edit" : "view" }}
+        </button>
+      </div>
+
       <textarea
           ref="titleArea"
-          class="w-screen max-w-screen-md text-2xl text-center resize-none"
+          class="w-screen max-w-screen-md font-bold text-4xl text-center resize-none"
           v-model="title"
           placeholder="edit me"
           :disabled="viewMode">
       </textarea>
-    </div>
-    <div class="flex justify-center">
-      <input type="text" v-model="category" :disabled="viewMode" class="text-center m-2 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700">
-    </div>
-    <div class="flex flex-row flex-wrap justify-center">
-      <p class="m-2">Creation date: {{ creation }}</p>
-      <p class="m-2">Last modification {{ lastmodify }}</p>
-    </div>
-    <div v-if="viewMode" class="flex justify-center">
-      <div v-html='renderedMarkdown' class="w-screen max-w-screen-md bg-white prose"></div>
-    </div>
-    <div v-else class="flex justify-center resize-none" >
+      <div class="flex justify-center">
+        <input type="text" v-model="category" :disabled="viewMode" class="text-center m-2 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700">
+      </div>
+      <!-- shof if only todoData is not empty -->
+      <div>
+        <todoList v-if="todoData.length > 0 || todoListShow" v-model="todoData" :editable="viewMode"></todoList>
+        <button v-else @click="todoListShow = !todoListShow" class="bg-yellow-400 rounded p-2">add todo list</button>
+      </div>
+      <div class="flex flex-row flex-wrap justify-center">
+        <p class="m-2">Creation date: {{ creation }}</p>
+        <p class="m-2">Last modification {{ lastmodify }}</p>
+      </div>
+      <div v-if="viewMode" class="flex justify-center">
+        <div v-html='renderedMarkdown' class="w-screen max-w-screen-md bg-white prose"></div>
+      </div>
+      <div v-else class="flex justify-center resize-none" >
       <textarea
           ref="contentArea"
           class="w-screen max-w-screen-md min-h-[200px] resize-none"
@@ -128,28 +134,26 @@ onMounted( async () => {
           placeholder="edit me"
           :disabled="viewMode">
       </textarea>
-    </div>
-
-    <div class="flex justify-center resize-none" >
-      <todoList v-model="todoData"></todoList>
-    </div>
-
-    <div class="flex flex-col justify-center flex-wrap w-screen max-w-screen-md m-auto">
-      <p class="text-center">Owners:</p>
-      <div class="flex">
-        <label for="open_to_anyone">open to anyone</label>
-        <input class="ml-1" type="checkbox" id="open_to_anyone" v-model="open_to_anyone" :disabled="viewMode"/>
       </div>
 
-      <VueMultiselect v-if="!open_to_anyone"
-          v-model="owners"
-          :options="users"
-          :multiple="true"
-          :disabled="viewMode">
-      </VueMultiselect>
+      <div class="flex flex-col justify-center flex-wrap w-screen max-w-screen-md m-auto">
+        <p class="text-center">Owners:</p>
+        <div class="flex">
+          <label for="open_to_anyone">open to anyone</label>
+          <input class="ml-1" type="checkbox" id="open_to_anyone" v-model="open_to_anyone" :disabled="viewMode"/>
+        </div>
 
+        <VueMultiselect v-if="!open_to_anyone"
+                        v-model="owners"
+                        :options="users"
+                        :multiple="true"
+                        :disabled="viewMode">
+        </VueMultiselect>
+
+      </div>
     </div>
   </div>
+
 </template>
 
 <style src="vue-multiselect/dist/vue-multiselect.css"></style>
