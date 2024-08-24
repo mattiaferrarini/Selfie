60681fb16b20b28856f4982b2460eceb2c9e3297
maforn <matteo.fornaini@studio.unibo.it>
2024-08-24 15:42:33 +0200

    Fixed tailwind config

--- selfie-client/public/progetti.js
+++ selfie-client/public/progetti.js
@@ -152,7 +152,7 @@ const unsubscribe = async () => {
 
 const logout = async () => {
     await unsubscribe();
-    await fetchWithMiddleware(`${API_URL}/logout`, {
+    await fetchWithMiddleware(`${API_URL}/auth/logout`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
