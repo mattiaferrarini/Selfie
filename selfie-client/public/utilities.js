e06de85bff6bd14161d4af60cc7b6a0888161158
Fabio Murer <81566084+zwirgioIlredelCodice@users.noreply.github.com>
2024-09-05 16:07:34 +0200

    origin/gantt-chart (#64)
    * Initialized projects modal
    
    * Updated linked activities and ids
    
    * Initalized addProject function
    
    * Added more server side functions
    
    * Edited server functions to reuse activityController and added status
    
    * torturato dai webcomponents
    
    * Added listing and status computing
    
    * Updated with editing of activities from actors
    
    * Fixed REST output and update
    
    * ganttComponent in progetti
    
    * better code organization with js modules
    
    * Fixed switch between gantt and list
    
    * gantt days bar
    
    * basic project gantt
    
    * info text
    
    * make gantt renderer recursive
    
    * add years and month bars
    
    * colors
    
    * current day-month-year underline and auto shrink-expand activities
    
    * overtime highlighted
    
    * minor fixes
    
    * style fixes
    
    * color legend
    
    * info fixed and chart overflow
    
    * closes #55; remove unused variables
    
    * re render gantt when date change (time machine)
    
    * fix time machine in project view and translate activity status to english
    
    * gantt now use right status
    
    * other time machine fixes
    
    * Fixed wrong abandoned calculation
    
    * orange instead of gold for late activities
    
    * cleanup and re-arrange code
    
    ---------
    
    Co-authored-by: maforn <matteo.fornaini@studio.unibo.it>

--- selfie-client/public/utilities.js
+++ selfie-client/public/utilities.js
@@ -40,3 +40,35 @@ export async function fetchWithMiddleware(url, options) {
     }
     return response;
 }
+
+export const getTimeMachineDate = () => {
+    let timeDifference = JSON.parse(localStorage.getItem('date')).realTimeDiff || 0;
+    return new Date(new Date().getTime() + timeDifference);
+}
+
+export const getStatusFromActivity = (activity, activities) => {
+    let status;
+    let linkedOutputUnavailable = false;
+    if (activity.linkedActivityId !== null) {
+        const linkedActivity = activities.find(act => act.localId === activity.linkedActivityId);
+        if (!(linkedActivity.output !== "" && linkedActivity.status === "Concluded"))
+            linkedOutputUnavailable = true;
+    }
+    // if two weeks have passed from the deadline or if the status is abandoned
+    if (activity.status === "Abandoned" || new Date(getTimeMachineDate().setDate(getTimeMachineDate().getDate() - 2 * 7)) > activity.activity?.deadline) {
+        status = "Abandoned";
+    } else if (activity.activity?.deadline < getTimeMachineDate() && (activity.output === "" || activity.status !== "Concluded")) {
+        status = "Late";
+    } else if (activity.input === "" || linkedOutputUnavailable) {
+        status = "Not activatable";
+    } else if (activity.status === "NotStarted") {
+        status = "Activatable";
+    } else if (activity.status === "Rejected") {
+        status = "Reactivated";
+    } else if (activity.status === "Concluded" && activity.output !== "") {
+        status = "Concluded";
+    } else {
+        status = "Active";
+    }
+    return status;
+}
