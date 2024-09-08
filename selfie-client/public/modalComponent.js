5e1aa065ffac51edf819f5c3dd1d623ff02d5ec8
maforn <matteo.fornaini@studio.unibo.it>
2024-09-08 16:59:10 +0200

    Fixed overflow and no padding in mobile view for modals

--- selfie-client/public/modalComponent.js
+++ selfie-client/public/modalComponent.js
@@ -4,7 +4,7 @@ class ModalComponent extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="tailwind.css"/>
-            <div class="fixed inset-0 items-center justify-center bg-black bg-opacity-30 hidden max-h-screen" id="container">
+            <div class="fixed inset-0 items-center justify-center bg-black p-1 sm:p-0 bg-opacity-30 hidden max-h-screen" id="container">
                 <div class="bg-white p-4 rounded-lg shadow-lg relative w-full max-w-2xl mx-auto">
                     <button class="absolute top-0 right-2 text-gray-500 text-lg" id="close-btn">&times;</button>
                     <slot></slot>
